a0040af1a8aa63b2a3f3797a7a1b6b6e6ba265b9
erisson.batista@ask.com <erissonbreno@hotmail.com>
2023-06-29 21:56:56 -0300

    Alterando para Java 11

--- src/main/java/br/com/acme/exceptions/ClienteAtrasadoException.java
+++ src/main/java/br/com/acme/exceptions/ClienteAtrasadoException.java
@@ -1,10 +1,7 @@
 package br.com.acme.exceptions;
 
-import java.io.Serial;
-
 public class ClienteAtrasadoException extends RuntimeException {
 
-    @Serial
     private static final long serialVersionUID = -8332142176853224884L;
 
     public ClienteAtrasadoException(String message) {
